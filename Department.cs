5966797b420d14319a1d9d91a96ddb71c1e86a97
Levan A <38010166+LevanAmashukeli@users.noreply.github.com>
2021-02-14 21:29:48 +0400

    Add files via upload

---
+++ Department.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organization_Employees_
+{
+   public class Department
+    {
+        public string DepartmentName { get; set; }
+        public int DepartmentId { get; set; }
+
+        public Department(string Name, int Id)
+        {
+            DepartmentName = Name;
+            DepartmentId = Id;
+        }
+
+        //public override string ToString()
+        //{
+        //    return $"{DepartmentName,15} {DepartmentId,3}";
+        //}
+    }
+}
+
---
+++ Worker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organization_Employees_
+{
+    public class Worker
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int DepartamentId { get; private set; }
+        public int DepartamentName {get; set; }
+        
+
+        public Worker(string Name, int age, int DepId)
+        {
+            this.Name = Name;
+            Age = age;
+            this.DepartamentId = DepId;
+        }
+
+        //public override string ToString()
+        //{
+        //    return $"{Name,7} {Age,3} {DepartamentId,3}";
+        //}
+    }
+}
